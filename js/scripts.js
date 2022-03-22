589cdd505c2daaff9bc28fffe2da5b28b9ba57c1
William Rodriguez <william@rodriguez1.com>
2022-03-22 10:40:26 -0700

    Add next and previous buttons to navigate list
    Co-Authored-by: Cameron Ray <Crayiphone@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
   $(".card-body").click(function(){
     $(this).children().toggleClass("display");
-
-
   });
 
+  $(".btn-next").click(function() {
+    $(".main-card").next(".card").toggleClass("main-card");
+    $(".main-card:first").toggleClass("main-card");
 
+    if ($(".main-card").length <= 0) {
+      $(".card:first").addClass("main-card");
+    }
+  });
+
+  $(".btn-prev").click(function() {
+    $(".main-card").prev(".card").toggleClass("main-card");
+    $(".main-card:last").toggleClass("main-card");
+
+    if ($(".main-card").length <= 0) {
+      $(".card:last").addClass("main-card");
+    }
+  });
 });
